71761cafa65d580ed26151b30ee9ca712bebda5b
Sarang <sarangdpatil2019@gmail.com>
2026-05-25 01:35:49 +0530

    Updated println

--- QuickSort.java
+++ QuickSort.java
@@ -29,8 +29,7 @@ public class QuickSort {
         quickSort(arr, 0, n-1);
 
         for(int i=0; i<n; i++){
-            System.out.print(arr[i] + " ");
+            System.out.println(arr[i] + " ");
         }
-        System.out.println();
     }
 }
