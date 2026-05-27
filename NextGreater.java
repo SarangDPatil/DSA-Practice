ada193623e2b5aecb1c62be043e8faa31a933754
Sarang <sarangdpatil2019@gmail.com>
2026-05-27 20:22:54 +0530

    First Attempt

---
+++ NextGreater.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+public class NextGreater {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 2, 25};
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                System.out.println(arr[i] + " -> -1");
+            } else {
+                System.out.println(arr[i] + " -> " +stack.peek());
+            }
+            stack.push(arr[i]);
+        }
+    }
+}
