7f5b77147a0947e02e5200a609c7d30cc898e419
Sarang <sarangdpatil2019@gmail.com>
2026-05-29 00:51:25 +0530

    First Attempt

---
+++ ValidParentheses.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+
+        String str = "{[()]}";
+
+        Stack<Character> stack = new Stack<>();
+        
+        boolean valid = true;
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    valid = false;
+                    break;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[')) {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+        if (!stack.isEmpty()) {
+            valid = false;
+        }
+
+        System.out.println(valid? "Valid": "Invalid"
+        );
+    }
+}
