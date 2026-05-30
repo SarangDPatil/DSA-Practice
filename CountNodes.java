26bfb4448dd2994a92e77fa732b03167237ad294
Sarang <sarangdpatil2019@gmail.com>
2026-05-31 00:24:26 +0530

    First Attempt

---
+++ CountNodes.java
@@ -0,0 +1,26 @@
+class CountNodes {
+
+    static int count(Node root) {
+
+        if(root == null)
+            return 0;
+
+        return 1 +
+               count(root.left) +
+               count(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(10);
+
+        root.left = new Node(20);
+        root.right = new Node(30);
+
+        root.left.left = new Node(40);
+
+        System.out.println(
+                count(root)
+        );
+    }
+}
