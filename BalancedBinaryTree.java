8d9427fcd42b8385f62ad38acf714547e3dfaa22
Sarang <sarangdpatil2019@gmail.com>
2026-06-04 00:30:10 +0530

    First Attempt

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,57 @@
+class TreeNode {
+
+    int data;
+    TreeNode left, right;
+
+    TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class BalancedBinaryTree {
+
+    static boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    static int checkHeight(TreeNode node) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        if (isBalanced(root)) {
+            System.out.println("Tree is Balanced");
+        } else {
+            System.out.println("Tree is Not Balanced");
+        }
+    }
+}
